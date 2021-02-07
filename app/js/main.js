f9bbb3acaf59f6d82897b51154c17796f9c8c715
SwampDiver <toashes88@gmail.com>
2021-02-07 11:27:48 +0700

    -

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,14 @@
 // import '../scss/style.scss';
 'use strict';
 
+//  $('#menu-toggle').click(function(){
+//   $(this).toggleClass('open');
+// })
 
+const menuButton = document.querySelector ('.toggle-button');
+
+menuButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  console.log('qqqqw');
+  menuButton.classList.toggle("toggle-button--open");
+});
