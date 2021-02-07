9c4d9420946e12cdb7b6bec8d8bc2ab514209fc7
SwampDiver <toashes88@gmail.com>
2021-02-07 15:32:29 +0700

    mobile menu 1.0

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,12 @@
 // import '../scss/style.scss';
 'use strict';
-
-//  $('#menu-toggle').click(function(){
-//   $(this).toggleClass('open');
-// })
-
+const mainMenu = document.querySelector ('.main-nav__list');
 const menuButton = document.querySelector ('.toggle-button');
+const logo = document.querySelector ('.page-header__logo');
 
 menuButton.addEventListener('click', (evt) => {
   evt.preventDefault();
-  console.log('qqqqw');
   menuButton.classList.toggle("toggle-button--open");
+  mainMenu.classList.toggle("main-nav__list--open");
+  logo.classList.toggle("page-header__logo--in-menu")
 });
